b4b696d960c93557587828c0cd247704a9f56d45
unknown <Michael Lau>
2020-05-03 23:16:27 -0400

    g

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace Fisher.Bookstore
                 options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext"))
             );
             services.AddScoped<IBooksRepository, BooksRepository>();
+            services.AddScoped<IAuthorsRepository, AuthorsRepository>();
             services.AddSingleton<IBooksRepository, TestBooksRepository>();
             services.AddSingleton<IAuthorsRepository, TestAuthorsRepository>();
         }
